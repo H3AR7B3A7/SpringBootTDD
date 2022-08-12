a0c46be4b2e16b00426044b91464d6837ded6e8f
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-08-13 01:34:33 +0200

    :sparkles: Added 404 test

--- src/test/java/be/dog/d/steven/springboottdd1/CarIT.java
+++ src/test/java/be/dog/d/steven/springboottdd1/CarIT.java
@@ -25,7 +25,7 @@ class CarIT {
         
         @Nested
         @DisplayName("When the car is present")
-        class CarExistsTest {
+        class ExistingCarTest {
             @Test
             @DisplayName("Then it is returned with a 200 status")
             void getCar_returnsCarDetails() {
@@ -38,6 +38,22 @@ class CarIT {
                 assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
                 assertThat(response.getBody().getName()).isEqualTo("prius");
                 assertThat(response.getBody().getType()).isEqualTo("hybrid");
+            }
+        }
+
+        @Nested
+        @DisplayName("When the car is NOT present")
+        class MissingCarTest {
+            @Test
+            @DisplayName("Then a 404 status is returned")
+            void getCar_returnsCarDetails() {
+                // arrange
+
+                // act
+                ResponseEntity<Car> response = restTemplate.getForEntity("/cars/tesla", Car.class);
+
+                // assert
+                assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
             }
         }
     }
